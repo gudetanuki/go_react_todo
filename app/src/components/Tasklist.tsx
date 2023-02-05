806b7038cd588a2efa0f736861e7aafd7bab609d
gudetanuki <kuukai.budda@gmail.com>
2023-02-05 23:03:14 +0900

    scssに変更

---
+++ app/src/components/Tasklist.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/Tasklist.css';
+
+function Tasklist() {
+  return (
+    <>
+      <input type="text" placeholder="タイトルを入力" />
+    </>
+  );
+}
+
+export default Tasklist;
